8dba64968d0a52d0b10cbab24ed0660bcae47a93
Paramjit Saikia <paramjitsaikia@gmail.com>
2025-11-27 00:46:51 +0530

    fix: update loading component to use responsive width classes

--- Frontend/src/utils/loading.js
+++ Frontend/src/utils/loading.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div className='w-screen h-screen'>
+    <div className='xl:w-[80%] w-[100vw] h-screen '>
         <div className="flex justify-center items-center w-48 h-48">
                 <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600"></div>
             </div>
